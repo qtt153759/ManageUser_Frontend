35e8d39dc62fa110fb8ebdc900e7d2e1d394bebc
qtt153759 <qtt153759@gmail.com>
2022-02-14 00:11:24 +0700

    Set up create-react-app react-router-dom bootstrap5 scss

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.scss";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/Login/Login";
+function App() {
+    return (
+        <Router>
+            <div className="app-container">
+                <Nav />
+                <Switch>
+                    <Route path="/about">about </Route>
+                    <Route path="/users">users </Route>
+                    <Route path="/" exact>
+                        home
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="*">404 not found</Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
